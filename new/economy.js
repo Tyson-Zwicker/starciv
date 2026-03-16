4150943e0094732fd017edd2554cecd2c1aa9a20
Tyson-Zwicker <tysonzwicker@gmail.com>
2026-03-16 19:26:04 -0300

    changed.

--- new/economy.js
+++ new/economy.js
@@ -1,136 +1,55 @@
-class Game {
-  static types = ['food', 'ore', 'gas', 'tech', 'manu'];
-  static {
-    this.max = {};
+export default class Economy {
+  calculateProduction(planet) { ; }
+  static calculateProduction(civ, planet, resource) {
+    // TODO: implement
+    return 0;
+  }
 
+  feedPopulation(planet) {
+    if (planet.population >= planet.stores.food) {
+      planet.stores.food -= planet.population;
+      return;
+    }
+    let required = planet.population - planet.stores.food;
+    let available = planet.stores.food + Math.min(planet.system.availableFreightersCount, required);
+    let transportsUsed = Math.min(required - planet.stores.food, planet.system.availableFreightersCount);
+    planet.system.availableFreightersCount -= transportsUsed;
+    return this.population - available;
   }
-}
-class Civ {
-  mod = {
-    food: 1,
-    ore: 1,
-    gas: 1,
-    tech: 1,
-    money: 1,
-    making: 1,
-    terraforming: 0.01,
-    infrastructure: 0.05,
-    workslot: 0.1
-  };
-  tech = 1;
-}
-//TODO:  Gates would be a StarSystem thing not a planet thing.
-//TODO:  StarSystems should have a mod for gate building (some easier to connect with than others)
-//Money "Stores" are a civ wide thing...
-class Planet {
-  mod = {
-    food: 1,
-    ore: 1,
-    gas: 1,
-    tech: 1,
-    money: 1,
-    making: 1,
-    terraforming: 0.01,
-    workslot: 0.1,
-    popGrowth: 1
-  };
-  infra = {
-    food: 1,
-    ore: 1,
-    gas: 1,
-    tech: 1,
-    money: 1
-  };
-  stores = {
+  static feedPopulation(planet) {
+    // TODO: implement
+    return 0;
+  }
 
+  static populationGrowth(planet) {
+    // TODO: implement
   }
-  population = 10;
-  maxSlots = {
-    food: this.population,
-    ore: this.population,
-    gas: this.population,
-    money: this.population,
-    tech: this.population
-  }
-}
-class Buildable {
-  static mod = { //the five basics are not included on this list.. they don't have this modifier..
 
-    terraforming: {
-      ore: 0.1,
-      gas: 1,
-      money: 1,
-      base: 0.001
-    },
-    ore_infra: {
-      ore: 1,
-      gas: 1,
-      money: 1,
-      base: 0.001
-    },
-    gas_infra: {
-      ore: 1,
-      gas: 1,
-      money: 1,
-      base: 0.001
-    },
-    food_infra: {
-      ore: 1,
-      gas: 1,
-      money: 1,
-      base: 0.001
-    },
-    tech_infra: {
-      ore: 1,
-      gas: 1,
-      money: 1,
-      base: 0.001
-    },
-    money_infra: {
-      ore: 1,
-      gas: 1,
-      money: 1,
-      base: 0.001
-    },
-    workslot: {
-      ore: 0.1,
-      gas: 1,
-      money: 1,
-      base: 0.001
-    },
-    autoslot: {
-      ore: 0.1,
-      gas: 1,
-      money: 1,
-      base: 0.001
-    },
-    orbital: {
-      ore: 0.1,
-      gas: 1,
-      money: 1,
-      base: 0.001
-    }
+  static starvation(surplusFood, planet) {
+    // TODO: implement
   }
-}
-class Work {
-  doWork(type, workers, civ, planet, res) {
-    prod = 0;
-    let frm1 = ['food', 'ore', 'gas', 'tech', 'money'];
-    let frm2 = ['food_infra', 'ore_infra', 'gas_infra', 'money_infra', 'tech_infra', 'terraforming', 'workerslot', 'autoslot'];
-    let frm3 = ['orbital', 'probe', 'freighter', 'transport', 'gate', 'ship1', 'ship2', 'defence'];
-    let civmod = 1;
-    if (civ.mod[type] !== undefined) civmod = civ.mod[type]
-    if (frm1.contains(type)) {//apply the first formula for production
-      prod = workers * planet.mod[type] * planet.infra[type] * civ[type] * civ.tech;
-    }
-    else if (frm2.contains(type)) { //apply the 2nd formula for production
-      prod = (res.ore * Buildable.mod[type].ore + res.gas * Buildable.mod[type].gas) * planet.mod[type] * Buildable.mod[type].base * civ.tech * racemod;
-    }
-    else if (frm3.contains(type)) { //..
-      prod = (res.ore * Buildable.mod[type].ore + res.gas * Buildable.mod[type].gas + res.money * Buildable.mod[type].money) * Buildable.mod[type].base * civ.tech * racemod;
-    } else {
-      throw new Error(`Unknown: [${type}]`);
-    }
-    return prod;
+
+  static groundProduction(planet) {
+    // TODO: implement
+  }
+
+  static orbitalProduction(planet) {
+    // TODO: implement
+  }
+
+  static payForInfrastructure(planet) {
+    // TODO: implement
+  }
+
+  static moveResources(system, availableFreightersCount) {
+    // TODO: implement
+  }
+
+  static collectSystemTaxes(system) {
+    // TODO: implement
+  }
+
+  static collectCivTaxes(civ) {
+    // TODO: implement
   }
 }

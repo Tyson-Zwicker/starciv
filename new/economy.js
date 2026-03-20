0a185ebee81b7ab02dc04c785735d05dd50f3f94
Tyson-Zwicker <tysonzwicker@gmail.com>
2026-03-20 20:26:07 -0300

    added notes on how money is supposed to work.

--- new/economy.js
+++ new/economy.js
@@ -1,5 +1,6 @@
 export default class Economy {
-  calculateProduction(planet) { ; }
+
+
   static calculateProduction(civ, planet, resource) {
     // TODO: implement
     return 0;
@@ -12,9 +13,23 @@ export default class Economy {
 
   static groundProduction(planet) {
     // TODO: implement
+    //Remember to diminish this if the civ has no money..
   }
 
   static orbitalProduction(planet) {
     // TODO: implement
+    //Remember to diminish this if the civ has no money..
   }
 }
+/* Money
+
+Money is collectivized at the system level first, then collected by the civ at the end of the turn
+The cost of infrastructure is calculated at the system level..
+
+When the civilization collects money from all the systems, it may inherit less than zero money (debt)
+
+Next turn the civizations money or lack there-of will affect all systems equally..
+The affect have having no money is that the infrastructures productivity is multiplied by the debtModifier:  money/infracost
+
+
+*/

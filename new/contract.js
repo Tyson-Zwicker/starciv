124247bc680709bf33068c36a73646255365dae1
Tyson-Zwicker <tysonzwicker@gmail.com>
2026-03-20 13:32:08 -0300

    tells freighters their route, which leg of the route they are on, and what they're supposed to be carrying.

--- new/contract.js
+++ new/contract.js
@@ -1,14 +1,20 @@
 export default class Contract {
-  //step: {destination, resource, amount}
+  //Direction 0 or 1 = GOING TO DESTINATION
+  //-1 = GOING BACK TO ORIGIN
+  //0 means it was a once way trip.
   phases = [];
   currentPhase = 0;
-  
-  static getTerms(contract) {
-    // TODO: implement
-    return contract?.terms ?? null;
-  }
+  static makeContract (origin, destination, resource, direction){
+    return {origin, destination, resource, amount, direction}
+  }  
+  //Returns false if it has no where to go..
   static nextPhase(contract) {
-    // TODO: implement
-    return false;
+    contract.direction *=-1;
+    if (contract.direction ===0) return false;
+    return true;
+  }
+  static getCurrentDestination(contract){
+    if (direction ===-1) return contract.origin;
+    return contract.destination;
   }
 }

0256ae34c560aa3bd4e91557e4a50ad445b9d180
Tyson-Zwicker <tysonzwicker@gmail.com>
2026-03-20 13:33:08 -0300

    added contract (only used by freighters)

--- new/ship.js
+++ new/ship.js
@@ -2,6 +2,7 @@ export default class Ship {
   static nextID = 0;
   ID = -1;
   frieghter = false;
+  contract = undefined; //Only Freighters have these
   owner = undefined;
   currentFleet = undefined;
 }

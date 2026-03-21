f60638918625f682f1291c680743b09ba2e75b59
Tyson-Zwicker <tysonzwicker@gmail.com>
2026-03-21 20:04:56 -0300

    converted to typescript.

---
+++ new/war.ts
@@ -0,0 +1,10 @@
+import {System} from './system';
+import {Fleet} from './fleet';
+
+export namespace War {
+	export function battle(_system:System, _attackingFleet:Fleet):boolean {
+		// TODO: resolve combat and return true if attacker wins
+		return false;
+	}
+
+}

f1014f1962ce634ab5fcdce594623830b653ec21
Tyson-Zwicker <tysonzwicker@gmail.com>
2026-03-23 18:10:48 -0300

    moved to economy

--- new/production.ts
+++
@@ -1,8 +0,0 @@
-export type ProducableGround = 'food' | 'ore' | 'gas' | 'tech' | 'money' | 'orbital';
-
-export const PRODUCABLEGROUND: ProducableGround[] = ['food', 'ore', 'gas', 'tech', 'money', 'orbital'];
-
-export type producableOrbital = 'orbital'|'probe'|'freighter'|'transport'|'gate'| 'defencePlatform';
-
-export const PRODUCABLEORBITAL: producableOrbital[] = ['orbital','probe','freighter','transport','gate', 'defencePlatform'];
-
--- new/resources.ts
+++
@@ -1,9 +0,0 @@
-export type ResourceTypes = 'food' | 'ore' | 'gas' | 'tech' | 'money';
-
-export const RESOURCETYPES: ResourceTypes[] = ['food', 'ore', 'gas', 'tech', 'money'];
-
-//Civ tech, Civ Natural talent, Planet resources, Planet Infrastructure, Planet WorkerSlots...  
-export type ResourceModifier = Record <ResourceTypes ,number>;
-
-//The amount of that resource stored per Planet/System/Civ/Freighter...
-export type ResourceCollection = Record<ResourceTypes, number>;
